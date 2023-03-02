60efbba7cf59d13917a01e8fb0828f0d8867151e
abusenet <88513508+abusenet@users.noreply.github.com>
2023-03-02 16:48:55 -0600

    feat: main handler for `fetch` command
    Needs the following environment variables:
    
    For Discord:
    
    - `DISCORD_APPLICATION_ID`: (required) to verify Discord interaction signature
    - `DISCORD_PUBLIC_KEY`: (required) to verify Discord interaction signature
    - `DISCORD_BOT_TOKEN`: to register Discord application command
    - `DISCORD_GUILD_ID`: to register Discord application command within that guild only
    
    If forwarding to external Debrid service, set `DEBRID_API_KEY`.
    
    Must set up `RCLONE_TARGET` to point to a Rclone's remote to upload files to.
    
    And any environment variables to configure Rclone. The followings are required if supporting Fshare:
    
    - `RCLONE_FSHARE_APP_KEY`
    - `RCLONE_FSHARE_USER_EMAIL`
    - `RCLONE_FSHARE_PASSWORD`

---
+++ main.ts
@@ -0,0 +1,187 @@
+#!/usr/bin/env -S deno run --allow-all
+
+import { router } from "https://raw.githubusercontent.com/sntran/hack-n-slash/main/mod.ts";
+import { fetch, Progress, Rclone } from "https://raw.githubusercontent.com/sntran/denolcr/rclone/main.ts";
+
+const encoder = new TextEncoder();
+// Rclone remote to upload files to, default to current directory.
+const TARGET = Deno.env.get("RCLONE_TARGET") || Deno.cwd();
+
+const HOSTERS = [
+  "1fichier.com",
+  "anonfiles.com",
+  "ddl.to",
+  "katfile.org",
+  "mega.nz",
+  "rapidgators.net",
+  "uptobox.com",
+  "zippyshare.com",
+];
+
+async function handleFetch(request: Request): Promise<Response> {
+  const authorization = request.headers.get("Authorization")!;
+  const [_user] = atob(authorization.split(" ")[1]).split(":");
+
+  const { searchParams } = new URL(request.url);
+  let source: string | URL = searchParams.get("url") || "";
+
+  let reply: BodyInit = "";
+
+  try {
+    source = new URL(source);
+  } catch (_error) {
+    return new Response(`Invalid URL`);
+  }
+
+  const host = source.host;
+
+  //#region AbortSignal
+  const handlerAbortController = new AbortController();
+  const signal = handlerAbortController.signal;
+  // Discord only allows 15 minutes for an interaction.
+  const abortTimeoutId = setTimeout(() => {
+    handlerAbortController.abort();
+  }, 15 * 60 * 1000); // 15 minutes
+  // Abort the request when the interaction is over.
+  request.signal?.addEventListener("abort", () => {
+    clearTimeout(abortTimeoutId);
+    handlerAbortController.abort();
+  });
+  //#endregion AbortSignal
+
+  //#region HOSTERS
+  // Grabs direct link from free hosters.
+  if (HOSTERS.includes(host)) {
+    const response = await fetch(
+      "https://debrid-link.com/api/v2/downloader/add",
+      {
+        method: "POST",
+        headers: {
+          "Authorization": `Bearer ${Deno.env.get("DEBRID_API_KEY")}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          url: `${source}`,
+          password: searchParams.get("password"),
+        }),
+        signal,
+      },
+    );
+    const { success, value } = await response.json();
+    if (!success) {
+      return new Response("Can't fetch");
+    }
+
+    source = new URL(value.downloadUrl);
+  }
+  //#endregion HOSTERS
+
+  // @TODO: Use Rclone's `--progress` instead when it's available.
+  reply = new ReadableStream({
+    async start(controller) { // When the stream starts
+      console.time(`${source}`);
+
+      let response;
+      if (host === "fshare.vn" || host === "www.fshare.vn") {
+        response = await Rclone.backend(
+          "download",
+          ":fshare:",
+          `${source}`,
+          {},
+        );
+      } else {
+        response = await Rclone.cat(`${source}`);
+      }
+
+      const { ok, status, statusText, headers, body, url } = response;
+
+      let { pathname } = new URL(url);
+      pathname = decodeURIComponent(pathname);
+      const filename = pathname.substring(pathname.lastIndexOf("/") + 1);
+
+      const fileSize = Number(headers.get("Content-Length"));
+
+      if (!ok) {
+        controller.enqueue(
+          encoder.encode(`\r**Error**: ${status} ${statusText}`),
+        );
+        return;
+      }
+
+      //#region Progress
+      function onProgress({
+        completedh,
+        done,
+        etah,
+        totalh,
+        percent,
+        rateh,
+        timeh,
+      }: Progress) {
+        let message = "\f"; // Clears previous message.
+        message +=
+          `\r\nTransferred:    ${completedh} / ${totalh}, ${percent}%, ${rateh}/s`;
+        if (!done) {
+          message += `, ETA ${etah}`;
+        }
+        message += `\r\nElapsed time:   ${timeh}`;
+        message += `\r\nTransferring:`;
+        message += `\r\n* ${filename}: ${percent}% /${totalh}, ${rateh}/s`;
+        if (!done) {
+          message += `, ${etah}`;
+        }
+
+        if (done) {
+          console.timeEnd(`${source}`);
+        }
+        controller.enqueue(encoder.encode(message));
+      }
+      //#endregion Progress
+
+      // Calls Rclone's `fetch` directly instead of `rcat` since it expects `Deno.stdin`.
+      fetch(`${TARGET}/${filename}`, {
+        method: "PUT",
+        // Hooks into the file stream and sends progress to reply stream.
+        body: body!
+          // Uses a `TransformStream` to terminate the stream when abort signal received.
+          .pipeThrough(
+            new TransformStream({
+              start(controller) {
+                signal.addEventListener("abort", () => {
+                  controller.terminate();
+                });
+              },
+            }),
+          )
+          .pipeThrough(new Progress(fileSize, onProgress)),
+      });
+    },
+  });
+
+  // @TODO: sends a "direct" link in the response for media to stream there.
+
+  return new Response(reply, {
+    headers: {
+      "Link": `</retry>; title="Retry"`,
+    },
+  });
+}
+handleFetch.displayName =
+  "Fetches a file from a remote `url`, with optional `password`.";
+
+export const routes = {
+  "/fetch?url=&password=": handleFetch,
+};
+
+export const handler = (options = {}) => router(routes, options);
+
+if (import.meta.main) {
+  const { serve } = await import("https://deno.land/std@0.178.0/http/server.ts");
+
+  const controller = new AbortController();
+
+  serve(handler(), {
+    port: Number(Deno.env.get("PORT")) || 9000,
+    signal: controller.signal,
+  });
+}
