c217a13dadb7b84c93ca693ec618f13d034452df
abusenet <88513508+abusenet@users.noreply.github.com>
2023-03-29 17:23:23 -0500

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export { router } from "https://raw.githubusercontent.com/sntran/hack-n-slash/main/mod.ts";
+export { router } from "https://raw.githubusercontent.com/sntran/web2bot/main/mod.ts";
 export {
   fetch,
   Progress,
   Rclone,
-} from "https://raw.githubusercontent.com/sntran/denolcr/rclone/main.ts";
+} from "https://raw.githubusercontent.com/sntran/denolcr/main/main.ts";
